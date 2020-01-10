1de12c40f2b9d514fc2db3c49011498ce14b02c6
alex <arodriguez1981@gmail.com>
2020-01-10 12:35:45 -0600

    Changes for review HTML (datos basicos)

--- notaria/settings.py
+++ notaria/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'notaria.main'
+    'notaria.main',
+    'notaria.datos_basicos'
 ]
 
 MIDDLEWARE = [
